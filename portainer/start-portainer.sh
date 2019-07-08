5e71384e73e396a666b9e02214efded62a52c94d
Eduardo Mioto <eduardo.mioto@gmail.com>
2019-07-08 20:20:13 -0300

    update bootstrap
    update bootstrap

--- portainer/start-portainer.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run -d -p 19000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
--- speed-dial/start-http-server.sh
+++
@@ -1,2 +0,0 @@
-#sudo docker run --name python_simplehttpserver -d -v /home/admin/speed-dial/:/var/www:ro -p 10000:8080 trinitronx/python-simplehttpserver
-sudo docker start python_simplehttpserver
